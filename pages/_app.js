0b540a509c0203c6375938f5e800cee1f716b188
Dmitri <dmitrisanzharov@gmail.com>
2022-01-23 22:52:39 +0000

    fixed footer in the signin page

--- pages/_app.js
+++ pages/_app.js
@@ -1,142 +1,137 @@
-import '../styles/globals.css'
-import Head from 'next/head';
-import {useEffect, useState} from 'react'; 
-import {AppProvider} from '../useContext';
-import Script from 'next/script'
+import "../styles/globals.css";
+import Head from "next/head";
+import { useEffect, useState } from "react";
+import { AppProvider } from "../useContext";
+import Script from "next/script";
 
 // component imports
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
-import FooterSecureSafeTransparentSection from '../components/FooterSecureSafeTransparentSection';
-
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import FooterSecureSafeTransparentSection from "../components/FooterSecureSafeTransparentSection";
+import FooterLowestSection from "../components/FooterLowestSection";
 
 function MyApp({ Component, pageProps }) {
+	const [domReady, setDomReady] = useState(false);
 
-  const [domReady, setDomReady] = useState(false); 
+	//********************************************************************
+	//          FUNCTIONS
+	// *******************************************************************
 
+	const checkPathNameAndDisplayFooter = () => {
+		if (location.pathname === "/sign-in") {
+			return false;
+		}
 
-//********************************************************************
-//          FUNCTIONS
-// *******************************************************************
+		if (location.pathname === "/user-session") {
+			return false;
+		}
 
-  const checkPathNameAndDisplayFooter = () => {
+		if (location.pathname === "/add-funds") {
+			return false;
+		}
 
-    if(location.pathname === '/sign-in'){
-      return false; 
-    }
+		if (location.pathname === "/test") {
+			return false;
+		}
 
-    if(location.pathname === '/user-session'){
-      return  false; 
-    }
+		if (location.pathname === "/transactions-reports") {
+			return false;
+		}
 
-    if(location.pathname === '/add-funds'){
-      return  false; 
-    }
+		if (location.pathname === "/" && sessionStorage.finLendersUserDataId) {
+			return false;
+		}
 
-    if(location.pathname === '/test'){
-      return false;
-    }
+		if (
+			/campaigns\/*/.test(location.pathname) &&
+			sessionStorage.finLendersUserDataId
+		) {
+			return false;
+		}
 
-    if(location.pathname === '/transactions-reports'){
-      return false;
-    }
+		if (location.pathname === "/invest-into-project") {
+			return false;
+		}
 
-    if(location.pathname === '/' && sessionStorage.finLendersUserDataId){
-      return false; 
-    }
+		if (location.pathname === "/my-investments") {
+			return false;
+		}
 
-    if(/campaigns\/*/.test(location.pathname) && sessionStorage.finLendersUserDataId){
-      return false; 
-    }
+		if (location.pathname === "/marketplace") {
+			return false;
+		}
 
-    if(location.pathname === '/invest-into-project'){
-      return false;
-    }
+		return <Navbar />;
+	};
 
-    if(location.pathname === '/my-investments'){
-      return false;
-    }
+	const showNavBar = () => {
+		return (
+			<>
+				{checkPathNameAndDisplayFooter() && <Navbar />}
+				{}
+			</>
+		);
+	};
 
-    if(location.pathname === '/marketplace'){
-      return false;
-    }
+	const showFooter = () => {
+		if (location.pathname === "/sign-in") {
+			return (
+				<>
+					<FooterSecureSafeTransparentSection />
+					<FooterLowestSection />
+				</>
+			);
+		}
 
+		return <>{checkPathNameAndDisplayFooter() && <Footer />}</>;
+	};
 
-    return <Navbar />
+	useEffect(() => {
+		setDomReady(true);
+	}, []);
 
-  }
+	return (
+		<>
+			<AppProvider>
+				<Head>
+					<link
+						href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+						rel="stylesheet"
+						integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+						crossOrigin="anonymous"
+					></link>
+					<link
+						rel="stylesheet"
+						href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css"
+					></link>
+					<link
+						href="https://api.mapbox.com/mapbox-gl-js/v2.3.1/mapbox-gl.css"
+						rel="stylesheet"
+					/>
+				</Head>
 
+				{domReady && showNavBar()}
 
+				<Component {...pageProps} />
 
-  const showNavBar = () => {
-    return (
-      <>
-      {checkPathNameAndDisplayFooter() && <Navbar />}
-      {}
-      </>
-    )
-  }
+				{domReady && showFooter()}
 
+				<Script
+					src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"
+					integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB"
+					crossOrigin="anonymous"
+				></Script>
 
-  const showFooter = () => {
+				<Script
+					src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
+					integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13"
+					crossOrigin="anonymous"
+				></Script>
 
-    if(location.pathname === '/sign-in'){
-      return <FooterSecureSafeTransparentSection />
-    }
-
-
-
-
-
-    return (
-      <>
-      {checkPathNameAndDisplayFooter() && <Footer />}
-      </>
-    )
-  }
-
-  useEffect(()=> {
-    setDomReady(true); 
-  }, [])
-
-
-
-  return (
-  
-  <>
-  <AppProvider>
-  <Head>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous"></link>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css"></link>
-<link href='https://api.mapbox.com/mapbox-gl-js/v2.3.1/mapbox-gl.css' rel='stylesheet' />
-
-  </Head>
-
-
-  {domReady && showNavBar()}
-
-  
-  <Component {...pageProps} />
-
-
-  {domReady && showFooter()} 
-
-
-  <Script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossOrigin="anonymous"></Script>
-
-<Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossOrigin="anonymous"></Script>
-
-<Script src="https://www.google.com/recaptcha/api.js"></Script>
-
-
-
-
-  </AppProvider>
-  </>
-  )
-  
-  
-  
+				<Script src="https://www.google.com/recaptcha/api.js"></Script>
+			</AppProvider>
+		</>
+	);
 }
 
-export default MyApp
+export default MyApp;
